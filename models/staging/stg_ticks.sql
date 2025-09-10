a093fbe7849e79a8aa6f9e91c0aec244b57baa32
vadireddy@umassd.edu <vadireddy@umassd.edu>
2025-09-10 22:11:29 +0000

    check_3

--- models/staging/stg_ticks.sql
+++ models/staging/stg_ticks.sql
@@ -6,16 +6,20 @@
 WITH src AS (
   SELECT
     event_ts,
-    LOWER(exchange) AS exchange,
-    UPPER(symbol)   AS symbol,
+    LOWER(exchange)        AS exchange,
+    UPPER(symbol)          AS symbol,
     IFF(side IN ('buy','sell'), side, NULL) AS side,
     TRY_TO_DOUBLE(price)    AS price,
     TRY_TO_DOUBLE(size)     AS size,
     TRY_TO_DOUBLE(best_bid) AS best_bid,
     TRY_TO_DOUBLE(best_ask) AS best_ask
   FROM {{ source('raw','crypto_ticks') }}
+  WHERE 1=1
   {% if is_incremental() %}
-    WHERE event_ts > (SELECT COALESCE(MAX(event_ts), '1970-01-01') FROM {{ this }})
+    AND event_ts > (
+      SELECT COALESCE(MAX(event_ts), TO_TIMESTAMP('1970-01-01'))
+      FROM {{ this }}
+    )
   {% endif %}
 )
 
@@ -30,4 +34,4 @@ SELECT
   best_ask,
   CONCAT(TO_VARCHAR(event_ts), '|', symbol, '|', exchange) AS event_ts_symbol_exchange
 FROM src
-WHERE price > 0 AND size >= 0;
+WHERE price > 0 AND size >= 0
